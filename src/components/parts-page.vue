9fbcef6ae278607a4f2aba5aa76af38160720ba7
Rodion Grishaev <radius486@gmail.com>
2018-04-03 02:58:28 +0300

    Parts page.

---
+++ src/components/parts-page.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="parts-page catalog-page" :style="{minHeight: ''+ blockHeight +'px'}">
+    <section class="catalog">
+      <div class="container">
+        <h2  class="catalog_title">Запчасти</h2>
+      </div>
+      <div class="container catalog-list">
+        <a v-if='parts' class="catalog-product" href='#' v-for='(part, index) in parts'>
+          <img class="catalog-product_image" :src='part.image'></img>
+          <h3 class="catalog-product_title">{{part.title}}</h3>
+          <p class="catalog-product_description">{{part.description}}</p>
+          <span class="catalog-product_more">Подробнее...</span>
+          <div class="catalog-product_price">{{part.price}}</div>
+        </a>
+        <p v-if='!parts' class="catalog-none">Нет в наличии</p>
+      </div>
+  </section>
+  </div>
+</template>
+
+<script>
+
+import $ from 'jquery';
+
+export default {
+  name: 'parts-page',
+
+  components: {
+  },
+
+  mounted() {
+    $(document).ready(() => {
+      this.blockHeight = window.innerHeight  - $('.header').outerHeight() - $('.footer').outerHeight();
+    });
+
+    $(window).on('resize', () => {
+      this.blockHeight = window.innerHeight  - $('.header').outerHeight() - $('.footer').outerHeight();
+    });
+  },
+
+  data () {
+    return {
+      blockHeight: 0
+    }
+  },
+
+  computed: {
+    parts() {
+      return this.$parent.app.parts;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='sass'>
+  @import '../assets/styles/components/catalog.sass'
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HomePage from '@/components/home-page.vue'
 import PrintersPage from '@/components/printers-page.vue'
 import PlasticPage from '@/components/plastic-page.vue'
+import PartsPage from '@/components/parts-page.vue'
 
 Vue.use(Router)
 
@@ -25,6 +26,12 @@ export default new Router({
       path: '/plastic',
       name: 'PlasticPage',
       component: PlasticPage
+    },
+
+    {
+      path: '/parts',
+      name: 'PartsPage',
+      component: PartsPage
     }
   ]
 })
