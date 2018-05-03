93ec00a90b34324afc3b98ecb35a1850048d7e13
Rodion Grishaev <radius486@gmail.com>
2018-05-04 01:27:14 +0300

    Back to scroll position fix.

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,7 @@ export default new Router({
 
       if(window.scrollHeight && (to.name != "HomePage") && (from.name == 'PlasticPage' || from.name == 'PrinterPage' || from.name == 'PenPage' || from.name == 'PartPage')) {
         scrollHeight = window.scrollHeight;
-        letency = 400;
+        letency = 320;
       }
 
       setTimeout(() => {
