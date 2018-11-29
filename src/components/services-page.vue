7bd5d3c3f8e6905b20b4edd50c7e037818f77c07
Rodion Grishaev <rodion.grishaev@thebigknow.com>
2018-11-29 15:10:25 +0300

    Gallery for services.

--- src/components/services-page.vue
+++ src/components/services-page.vue
@@ -12,15 +12,48 @@
           <img class="services_image" :src='services.image'>
         </div>
       </section>
+
+      <section v-if='services' class="gallery">
+        <h2 class="gallery-title">{{services.gallery_title}}</h2>
+
+        <div class='gallery-list'>
+          <div
+            v-for='item in services.gallery'
+            class="gallery-item"
+            :style="{backgroundImage: 'url(' + item + ')'}"
+            @click.prevent='openImage(item)'></div>
+        </div>
+
+        <sweet-modal
+          class='gallery-overlay'
+          modal-theme='white'
+          overlay-theme='dark'
+          ref='galleryOverlay'
+          @close='closeImage'>
+          <img :src='current_image'>
+        </sweet-modal>
+      </section>
+
       <p v-if='!services' class="services-none">Услуги не найдены</p>
     </div>
   </div>
 </template>
 
 <script>
+import { SweetModal } from 'sweet-modal-vue';
 
 export default {
   name: 'services-page',
+
+  data() {
+    return {
+      current_image: null
+    }
+  },
+
+  components: {
+    SweetModal
+  },
 
   computed: {
     services() {
@@ -35,6 +68,17 @@ export default {
       this.$parent.productName = this.services.title;
       this.$parent.productColor = this.services.color;
       this.$router.push('/#contacts');
+    },
+
+    openImage(image) {
+      this.$refs.galleryOverlay.open()
+      this.current_image = image
+    },
+
+    closeImage() {
+      setTimeout(() => {
+        this.current_image = null
+      }, 300)
     }
   }
 }
